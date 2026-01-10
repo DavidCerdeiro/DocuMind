704ae3a27817159569d1bc94f3206eaa26db437b
daviddcer22 <davidcergall22@gmail.com>
2026-01-10 15:58:17 +0100

    chore: backend initialization and db connection

---
+++ backend/src/test/java/com/davidcerdeiro/documind/DocumindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcerdeiro.documind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

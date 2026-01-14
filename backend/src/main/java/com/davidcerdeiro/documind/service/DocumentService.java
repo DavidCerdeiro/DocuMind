d24c052ba64ef7a8c4d75655014ee956ac3ab10a
daviddcer22 <davidcergall22@gmail.com>
2026-01-14 17:05:49 +0100

    backend: add service to chunk documents

---
+++ backend/src/main/java/com/davidcerdeiro/documind/service/DocumentService.java
@@ -0,0 +1,37 @@
+package com.davidcerdeiro.documind.service;
+
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentService {
+    
+    @Value("${app.document.chunk-size}")
+    public int chunkSize;
+
+    @Value("${app.document.chunk-overlap}")
+    public int chunkOverlap;
+
+    // Method to chunk PDF document
+    List<Document> chunkingDocument(Resource document) {
+        // First, read the PDF document
+        PagePdfDocumentReader pdfDocumentReader = new PagePdfDocumentReader(document);
+        List<Document> documents = pdfDocumentReader.read();
+
+        // Clean documents by trimming whitespace
+        List<Document> cleanedDocuments = documents.stream()
+                .map(doc -> new Document(doc.getText().trim(), doc.getMetadata()))
+                .toList();
+
+        // Now, chunk the cleaned documents
+        TokenTextSplitter textSplitter = new TokenTextSplitter(chunkSize, chunkOverlap, 5, 10000, true);
+        
+        return textSplitter.apply(cleanedDocuments);
+    }
+}
